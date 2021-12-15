eedb788ac491da2f5705b5ed1fccac94031e9e3a
KuberDriver <28806564+KuberDriver@users.noreply.github.com>
2021-12-15 20:32:43 +0000

    Testing CI with forked python repo

--- run_tox.sh
+++ run_tox.sh
@@ -36,6 +36,7 @@ popd > /dev/null
 cd "${TMP_DIR}"
 git clone https://github.com/KuberDriver/python.git #https://github.com/kubernetes-client/python.git
 cd python
+git remote show origin
 git config user.email "kubernetes-client@k8s.com"
 git config user.name "kubenetes client"
 git rm -rf kubernetes/base
